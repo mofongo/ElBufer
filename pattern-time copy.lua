60a3b26364c31386f71d00dee18ccc174a58e7ba
Jose Ayala <mofongo1@gmail.com>
2025-07-10 11:43:36 -0400

    name changes

--- pattern-time copy.lua
+++ pattern-time copy.lua
@@ -1,4 +1,4 @@
-pattern_time = require 'pattern_time'
+pattern_time = require 'pattern_time' -- use the pattern_time lib in this script
 
 tau = math.pi * 2
 VOICES = 4
@@ -9,13 +9,14 @@ hold = false
 REFRESH_RATE = 0.03
 
 function init()
-  enc_pattern = pattern_time.new()
-  enc_pattern.process = parse_enc_pattern
+  enc_pattern = pattern_time.new() -- establish a pattern recorder
+  enc_pattern.process = parse_enc_pattern -- assign the function to be executed when the pattern plays back
 
-  enc_value = {0, 0, 0, 0} -- Track 4 encoders
+  enc_value = 0
   pattern_message = "press K3 to start recording"
   erase_message = "(no pattern recorded)"
   overdub_message = ""
+
 
   screen_dirty = true
   screen_timer = clock.run(
@@ -34,60 +35,68 @@ end
 function record_enc_value()
   enc_pattern:watch(
     {
-      ["value"] = {enc_value[1], enc_value[2], enc_value[3], enc_value[4]}
+      ["value"] = enc_value
     }
   )
 end
 
 function parse_enc_pattern(data)
-  enc_value = {data.value[1], data.value[2], data.value[3], data.value[4]}
+  enc_value = data.value
   screen_dirty = true
 end
 
 function key(n,z)
   if n == 3 and z == 1 then
-    if enc_pattern.rec == 1 then
-      enc_pattern:rec_stop()
-      enc_pattern:start()
+    if enc_pattern.rec == 1 then -- if we're recording...
+      enc_pattern:rec_stop() -- stop recording
+      enc_pattern:start() -- start playing
       pattern_message = "playing, press K3 to stop"
       erase_message = "press K2 to erase"
       overdub_message = "hold K1 to overdub"
-    elseif enc_pattern.count == 0 then
-      enc_pattern:rec_start()
+    elseif enc_pattern.count == 0 then -- otherwise, if there are no events recorded..
+      enc_pattern:rec_start() -- start recording
       record_enc_value()
       pattern_message = "recording, press K3 to stop"
       erase_message = "press K2 to erase"
       overdub_message = ""
-    elseif enc_pattern.play == 1 then
-      enc_pattern:stop()
+    elseif enc_pattern.play == 1 then -- if we're playing...
+      enc_pattern:stop() -- stop playing
       pattern_message = "stopped, press K3 to play"
       erase_message = "press K2 to erase"
       overdub_message = ""
-    else
-      enc_pattern:start()
+    else -- if by this point, we're not playing...
+      enc_pattern:start() -- start playing
       pattern_message = "playing, press K3 to stop"
       erase_message = "press K2 to erase"
       overdub_message = "hold K1 to overdub"
     end
   elseif n == 2 and z == 1 then
-    enc_pattern:rec_stop()
-    enc_pattern:stop()
-    enc_pattern:clear()
+    enc_pattern:rec_stop() -- stops recording
+    enc_pattern:stop() -- stops playback
+    enc_pattern:clear() -- clears the pattern
     erase_message = "(no pattern recorded)"
     pattern_message = "press K3 to start recording"
     overdub_message = ""
   elseif n == 1 then
-    enc_pattern:set_overdub(z)
+    enc_pattern:set_overdub(z) -- toggles overdub
     overdub_message = z == 1 and "overdubbing" or "hold K1 to overdub"
   end
   screen_dirty = true
 end
 
+-- function enc(n,d)
+--   if n == 3 then
+--     enc_value = enc_value + d
+--     record_enc_value()
+--     screen_dirty = true
+--   end
+-- end
+
 a = arc.connect()
 
 a.delta = function(n,d)
-  if n >= 1 and n <= 4 then
-    enc_value[n] = enc_value[n] + d
+  if n == 1 then
+    enc_value = enc_value + d
     record_enc_value()
     screen_dirty = true
   end
@@ -95,9 +104,7 @@ end
 
 arc_redraw = function()
   a:all(0)
-  for i=1,4 do
-    a:segment(i, 0.5, 0.5 + enc_value[i]/100, 15)
-  end
+  a:segment(1,0.5,0.5+enc_value/100,15)
   a:refresh()
 end
 
@@ -111,15 +118,13 @@ re:start()
 function redraw()
   screen.clear()
   screen.level(15)
-  for i=1,4 do
-    screen.move(0,10*i)
-    screen.text("encoder "..i.." value: "..enc_value[i])
-  end
-  screen.move(0,50)
+  screen.move(0,10)
+  screen.text("encoder 3 value: "..enc_value)
+  screen.move(0,40)
   screen.text(pattern_message)
-  screen.move(0,60)
+  screen.move(0,50)
   screen.text(erase_message)
-  screen.move(0,70)
+  screen.move(0,60)
   screen.text(overdub_message)
   screen.update()
 end
